380536eb29713a4dd153e4f54c34993ccfb58255
Medliarskyii <63199632+Medliarskyii@users.noreply.github.com>
2020-10-02 20:34:43 +0300

    Add files via upload

---
+++ DTO/SecurityDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DTO
+{
+     public class SecurityDTO
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+
+        public string Password { get; set; }
+        public string KeyWord { get; set; }
+        public bool IsTemporary { get; set; }
+
+        public DateTime ExpiredDate { get; set; }
+
+    }
+}
---
+++ DTO/UserDTO.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace DTO
+{
+    public class UserDTO
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+        public string Gender { get; set; }
+        public string Address { get; set; }
+
+        public int RoleId { get; set; }
+        public string Mail { get; set; }
+        public string Tel { get; set; }
+        public string BankCard { get; set; }
+
+
+        public string GetInfo()
+        {
+            string info = Id.ToString() + " " + FirstName + " " + LastName + " " + Gender + " " + Address + " " + Mail + " " + Tel + " " + BankCard;
+            return info;
+        }
+
+
+    }
+    
+
+}
