652237a3e2e65be314c8d85627422cd1b92fc45c
Medliarskyii <63199632+Medliarskyii@users.noreply.github.com>
2020-10-02 23:53:26 +0300

    Add files via upload

---
+++ UnitTestProject/UnitTest1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DAL;
+using DTO;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethodCheckUser()
+        {
+            string connStr = "Data Source=DESKTOP-KHVC2BC;Initial Catalog=PZ_K;Integrated Security=True";
+            UserDAL dal = new UserDAL(connStr);
+            UserDTO currentUser = null;
+            currentUser = dal.GetUserById(4);
+
+            Assert.AreEqual(currentUser.FirstName, "wom");
+        }
+
+        [TestMethod]
+        public void TestMethodCheckUserUpdate()
+        {
+            string connStr = "Data Source=DESKTOP-KHVC2BC;Initial Catalog=PZ_K;Integrated Security=True";
+            UserDAL dal = new UserDAL(connStr);
+            UserDTO currentUser = null;
+            currentUser = dal.GetUserById(4);
+            currentUser.FirstName = "wom1";
+            currentUser = dal.UpdateUser(currentUser);
+
+            Assert.AreEqual(currentUser.FirstName, "wom1");
+        }
+
+
+        [TestMethod]
+        public void CheckUsersCount()
+        {
+            string connStr = "Data Source=DESKTOP-KHVC2BC;Initial Catalog=PZ_K;Integrated Security=True";
+            UserDAL dal = new UserDAL(connStr);
+            int count = dal.GetAllUsers().Count;
+
+            Assert.AreEqual(count,3);
+        }
+        
+
+    }
+}
