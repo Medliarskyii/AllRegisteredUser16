266490eb19bb3736b362135c9482413ccbc784eb
Medliarskyii <63199632+Medliarskyii@users.noreply.github.com>
2020-11-10 21:25:28 +0200

    Add files via upload

---
+++ WindowsForm/UnitTestBL/UnitTestBL.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BL;
+
+namespace UnitTestBL
+{
+    [TestClass]
+    public class UnitTestBL
+    {
+        [TestMethod]
+        public void TestGetCurrentUser()
+        {
+           
+            Assert.AreEqual(LoginBL.SetCurrentUser("@gmail234"), false);
+            Assert.AreEqual(LoginBL.SetCurrentUser("@gmail"), true);
+            LoginBL.SetCurrentUser("@gmail");
+            Assert.AreEqual(true,LoginBL.ChekPasswordForCurrentUser("1234"));
+            Assert.AreEqual(false, LoginBL.ChekPasswordForCurrentUser("12349898"));
+        }
+    }
+}
