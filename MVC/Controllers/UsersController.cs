292466723f1b0d934c278cfa17dbe920e17a9f80
Medliarskyii <63199632+Medliarskyii@users.noreply.github.com>
2020-12-08 16:19:00 +0200

    Add files via upload

---
+++ MVC/Controllers/UsersController.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using WebCoreKino5.Models;
+using Microsoft.AspNetCore.Http;
+using System.IO;
+
+namespace WebCoreKino5.Controllers
+{
+    public class UsersController : Controller
+    {
+        private readonly PZ_KContext _context;
+
+        public static int? LogedUserId = 0;
+
+        public UsersController(PZ_KContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Users
+        public async Task<IActionResult> Index()
+        {
+            var pZ_KContext = _context.Users.Include(u => u.Role);
+            ViewBag.LogedUserId = LogedUserId;
+            return View(await pZ_KContext.ToListAsync());
+        }
+
+        // GET: Users/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            PZ_KContext.ViewMode = SiteViewMode.User;
+
+            if (LogedUserId != 0 && id != LogedUserId)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+
+            LogedUserId = id;
+
+
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _context.Users
+                .Include(u => u.Role)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return View(user);
+        }
+
+        // GET: Users/Create
+        public IActionResult Create()
+        {
+            ViewData["RoleId"] = new SelectList(_context.Roles, "Id", "Name");
+            return View();
+        }
+
+        // POST: Users/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Surname,Mail,Password,KeyWord,ExpiredDate,IsTemporary,Image,Image2,Description,RoleId")] User user)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(user);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["RoleId"] = new SelectList(_context.Roles, "Id", "Name", user.RoleId);
+            return View(user);
+        }
+
+        // GET: Users/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id != LogedUserId)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+
+            var user = await _context.Users.FindAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            ViewData["RoleId"] = new SelectList(_context.Roles, "Id", "Name", user.RoleId);
+
+            UserExt ue = new UserExt(user);
+
+            return View(ue);
+        }
+
+
+
+
+
+        // POST: Users/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Surname,Mail,Password,KeyWord,ExpiredDate,IsTemporary,Image,Avatar,Image2,Description,RoleId")] UserExt ue)
+        {
+            User user = ue.GetUser();
+
+            if (id != user.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(user);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!UserExists(user.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+
+          
+
+            ViewData["RoleId"] = new SelectList(_context.Roles, "Id", "Name", user.RoleId);
+        
+            return View(user);
+        }
+
+
+
+        // GET: Users/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id != LogedUserId)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _context.Users
+                .Include(u => u.Role)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return View(user);
+        }
+
+        // POST: Users/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var user = await _context.Users.FindAsync(id);
+            _context.Users.Remove(user);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool UserExists(int id)
+        {
+            return _context.Users.Any(e => e.Id == id);
+        }
+
+        // GET: Users/Create
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        // POST: Users/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login([Bind("Mail,Password")] User userLoginInfo)
+        {
+            string login;
+            string password;
+           
+            foreach (User u in _context.Users)
+            {
+                login = u.Mail.Replace(" ", "");
+                password = u.Password.Replace(" ", "");
+
+                if (login == userLoginInfo.Mail && password == userLoginInfo.Password)
+                {
+                    return RedirectToAction("Details", "Users", new { id = u.Id });
+                }
+
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
